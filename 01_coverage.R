2719488f1709d0f62e5a0d6200bf97d1efb62c4f
Rosemary McCloskey <rmcclosk.math@gmail.com>
2014-09-03 19:19:54 -0700

    Got coverage script working.

--- 01_coverage.R
+++ 01_coverage.R
@@ -5,10 +5,10 @@ source(file="settings.R")
 dir.create(coverage.dir, showWarnings=F)
 matching <- read.table(matching.file, stringsAsFactors=F, fill=T, sep="\t")
 files <- c(matching$V1, matching$V2, matching$V3)
-files
-quit()
+files <- files[!files %in% c("", "Insufficient DNA")]
+files <- sapply(files, function (f) strsplit(f, " ")[[1]][2])
 
-main <- sapply(matching[,c(2,4,6)], function (bam.file) {
+main <- sapply(files, function (bam.file) {
     bam.path <- file.path(bam.dir, bam.file)
     coverage.path <- file.path(coverage.dir, sub(".bam$", "", bam.file))
     if (! file.exists(paste0(coverage.path, ".sample_summary"))) {
