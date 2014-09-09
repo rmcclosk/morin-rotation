ad970398576b3fc71a105d71ac9d7cbe1a7c4cae
Rosemary McCloskey <rmcclosk.math@gmail.com>
2014-09-09 08:40:45 -0700

    Finalized coverage calculations.

--- 02_coverage.sh
+++ 02_coverage.sh
@@ -9,7 +9,7 @@ mkdir -p $OUTDIR
 for BAM_FILE in $(ls $INDIR/*exome*.bam); do
     OUTFILE_STEM=$OUTDIR/$(basename $BAM_FILE | sed s/.bam$//)
     if [[ ! -f $OUTFILE_STEM.sample_summary ]]; then
-        echo "$JAVA_BIN -Xmx1024m -jar $GATK_JAR -T DepthOfCoverage -omitBaseOutput \
+        echo "$JAVA_BIN -Xmx2048m -jar $GATK_JAR -T DepthOfCoverage -omitBaseOutput \
             -omitLocusTable -R $HUMAN_REF -I $BAM_FILE -L $INTERVAL_LIST \
             -o $OUTFILE_STEM"
     else
@@ -19,5 +19,5 @@ done > jobs.txt
 
 exit 0
 if [[ -s jobs.txt ]]; then
-    mqsub --file jobs.txt --chdir qsub-logs --name coverage --qsub "-l h_vmem=2G -l mem_token=2G -l mem_free=2G"
+    mqsub --file jobs.txt --chdir qsub-logs --name coverage --qsub "-l h_vmem=3G -l mem_token=3G -l mem_free=3G"
 fi
