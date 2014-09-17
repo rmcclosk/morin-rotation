f2c39be33f7f30981f5afd9f5eec31922d08e15b
Rosemary McCloskey <rmcclos@sfu.ca>
2014-09-17 08:47:55 -0700

    Check for missing read group in BAMs.

--- 01_fixbams.sh
+++ 01_fixbams.sh
@@ -14,12 +14,23 @@ for EXOME_BAM in $(ls $IN_DIR/*.bam); do
     OUT_FILE=$OUT_DIR/$(basename $EXOME_BAM)
     if [[ ! -f $OUT_FILE ]]; then
         samtools view -H $EXOME_BAM > header.sam
+
+        # Check for missing version number.
         grep 'VN:\W' header.sam > /dev/null
-        BAD_NV=$([[$? -eq 0]])
-        echo $BAD_NV
-        continue
-        if [[ $? -eq 0 ]]; then
-            sed 's/VN:\t/VN:0.5.7\t/' header.sam > $OUT_FILE.header
+        GOOD_VN=$?
+
+        grep 'RG' header.sam > /dev/null
+        MISSING_RG=$?
+
+        if [[ $GOOD_VN -eq 0 ]]; then
+            sed -i 's/VN:\t/VN:0.5.7\t/' header.sam 
+        fi
+        if [[ $MISSING_RG -eq 1 ]]; then
+            echo "@RG\tID:1" >> header.sam
+        fi
+
+        if [[ $GOOD_VN -eq 0 || $MISSING_RG -eq 1 ]]; then
+            cp header.sam $OUT_FILE.header
             echo -n "source $HOME/.bash_profile; "
             echo -n "samtools reheader $OUT_FILE.header "
             echo "$(readlink -f $EXOME_BAM) > $OUT_FILE" 
@@ -30,7 +41,7 @@ for EXOME_BAM in $(ls $IN_DIR/*.bam); do
     else
         echo "Output file $OUT_FILE already exists" >&2
     fi
-done #> jobs.txt
+done > jobs.txt
 exit 0
 
 if [[ -s jobs.txt ]]; then
