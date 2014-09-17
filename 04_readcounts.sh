5596dd2bdf2993d1c95b68bb1cf582a42fc88e24
Rosemary McCloskey <rmcclosk.math@gmail.com>
2014-09-17 13:56:23 -0700

    Readcounts only for genome files.

--- 04_readcounts.sh
+++ 04_readcounts.sh
@@ -8,8 +8,10 @@ mkdir -p $OUT_DIR
 
 rm -f jobs.txt && touch jobs.txt
 
-for BAM_FILE in $(ls $IN_DIR/*genome*.bam); do
+for BAM_FILE in $(ls $IN_DIR/*.bam); do
     OUT_FILE=$OUT_DIR/$(basename $BAM_FILE | sed s/.bam$/.wig/)
+    if [[ $BAM_FILE == *exome* ]]; then continue; fi
+
     if [[ ! -f $OUT_FILE ]]; then
         echo "$HMMCOPY_DIR/readCounter -c 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,X,Y $BAM_FILE > $OUT_FILE" >> jobs.txt
     else
