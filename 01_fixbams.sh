591894ac87a738129446f05dba03175200ec41df
Rosemary McCloskey <rmcclosk.math@gmail.com>
2014-09-17 18:44:51 -0700

    Fix missing read groups (see #16).

--- 01_fixbams.sh
+++ 01_fixbams.sh
@@ -16,34 +16,28 @@ for EXOME_BAM in $(ls $IN_DIR/*.bam); do
         samtools view -H $EXOME_BAM > header.sam
 
         # Check for missing version number.
-        grep 'VN:\W' header.sam > /dev/null
-        GOOD_VN=$?
-
-        grep 'RG' header.sam > /dev/null
-        MISSING_RG=$?
-
-        if [[ $GOOD_VN -eq 0 ]]; then
-            sed -i 's/VN:\t/VN:0.5.7\t/' header.sam 
+        grep "VN:   " header.sam > /dev/null
+        if [[ $? -eq 0 ]]; then
+            echo "Replacing empty version flag with 0.5.7" >&2
+            sed -i 's/VN:/VN:0.5.7/' header.sam
+            samtools reheader header.sam $EXOME_BAM > $OUT_FILE
         fi
-        if [[ $MISSING_RG -eq 1 ]]; then
-            echo "@RG\tID:1" >> header.sam
-        fi
-
-        if [[ $GOOD_VN -eq 0 || $MISSING_RG -eq 1 ]]; then
-            cp header.sam $OUT_FILE.header
+    
+        # Check for inconsistency in read groups between body and header.
+        BODY_RG=$(samtools view $EXOME_BAM | head -n 100 | grep -o "RG:Z:[^  ]*" | sort | uniq | cut -d ':' -f 3)
+        HEAD_RG=$(grep RG header.sam | cut -f 2 | cut -d ':' -f 2)
+        if [[ "x$HEAD_RG" -ne "x$BODY_RG" ]]; then
             echo -n "source $HOME/.bash_profile; "
-            echo -n "samtools reheader $OUT_FILE.header "
-            echo "$(readlink -f $EXOME_BAM) > $OUT_FILE" 
-        else
-            ln -s $(readlink -f $EXOME_BAM) $OUT_FILE
-            ln -s $(readlink -f $EXOME_BAM.bai) $OUT_FILE.bai
+            echo -n "java -Xmx2048m -jar $PICARD_DIR/AddOrReplaceReadGroups.jar "
+            echo -n "INPUT=$EXOME_BAM OUTPUT=$OUT_FILE.regrouped.bam RGLB=N/A "
+            echo -n "RGPL=Ilumina RGPU=N/A RGSM=N/A"
         fi
     else
         echo "Output file $OUT_FILE already exists" >&2
     fi
 done > jobs.txt
-exit 0
 
+exit 0
 if [[ -s jobs.txt ]]; then
     mqsub --file jobs.txt --chdir qsub-logs --name fixbams
 fi
