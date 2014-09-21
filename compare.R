3487e93034e54d8455f7b39b2408c3107057532f
Rosemary McCloskey <rmcclosk.math@gmail.com>
2014-09-21 16:46:03 -0700

    Fix compare script to order segments properly.

--- compare.R
+++ compare.R
@@ -2,36 +2,39 @@
 
 source(file="settings.conf")
 
-sample.data <- read.csv("BC Genome Profiling-HQC.csv", header=T, fill=T, stringsAsFactors=F)
-sample.data$purity <- (sample.data$X..tumor/100) * 
-                      ((sample.data$X..viable.neoplastic.cells/100) +
-                       (sample.data$X..necrosis/100))
-sample.data$purity <- ifelse(is.na(sample.data$purity), 
-                             sample.data$X..Tumor/100, 
-                             sample.data$purity)
-sample.data <- sample.data[,c("Sample.ID", "purity")]
-colnames(sample.data) <- c("sample", "report.purity")
-sample.data <- sample.data[grepl("^01", sample.data$sample),]
-sample.data$patient.id <- paste0("01-", sapply(strsplit(sample.data$sample, "-"), "[[", 3))
-sample.data$report.purity <- sub(".0$", "", floor((sample.data$report.purity)/0.05)*0.05)
+rep.ends <- function (x) {
+    c(head(x, 1), x, tail(x, 1))
+}
 
 seg.data <- read.table("annotations.dat", header=T)
 
-apply(sample.data, 1, function (row) {
-    pdf(file.path("plots"), paste0(sample, ".pdf")), width=9, height=9)
-    par(mfrow=c(2, 2), xaxs="i")
+# remove segments likely to cause error
+seg.data$arm <- substr(seg.data$locus, 1, 1)
+seg.data <- subset(seg.data, (! chr %in% c(13, 14, 15, 21, 22)) | arm != "p")
+seg.data <- subset(seg.data, chr != "Y")
 
-    sapply(c(1:22, "X", "Y"), function (plot.chr) {
-        plot.seg <- subset(seg.data, sample==row["sample"] & 
-                                     purity==row["report.purity"] &
-                                     chr==plot.chr)
-        genome.fun <- stepfun(plot.seg$start, c(0, plot.seg$genome.copy))
-        exome.fun <- stepfun(plot.seg$start, c(0, plot.seg$exome.copy))
+# use only the purity closest to the pathologist's estimate
+purity.to.use <- do.call(rbind, by(seg.data, seg.data$sample, function (x) {
+    x[which.min(abs(x$purity-x$report.purity)), c("sample", "purity")]
+}, simplify=F))
+seg.data <- merge(seg.data, purity.to.use)
+
+by(seg.data, seg.data$sample, function (by.seg.data) {
+    pdf(file.path("plots", paste0(by.seg.data[1,"sample"], ".pdf")), width=9, height=9)
+    par(mfrow=c(2, 2))
+
+    sapply(c(1:22, "X"), function (plot.chr) {
+        plot.seg <- subset(by.seg.data, chr==plot.chr)
+        plot.seg <- plot.seg[order(plot.seg$start),]
+        bounds <- c(plot.seg$start, tail(plot.seg$end, 1))
+        genome.fun <- stepfun(bounds, rep.ends(plot.seg$genome.copy)+0.02)
+        exome.fun <- stepfun(bounds, rep.ends(plot.seg$exome.copy)-0.02)
     
         plot(genome.fun, col="red", do.points=F,
              main="",
              xlab=paste("Position on chromosome", plot.chr),
-             ylab="Copy number", lwd=2, xlim=c(min(segments$start), max(segments$end)))
+             ylab="Copy number", lwd=2, xlim=c(min(bounds), max(bounds)),
+             xaxs="i", ylim=c(0,5), yaxs="i")
         plot(exome.fun, col="blue", do.points=F, add=T, lwd=2)
 
         xmax <- par("usr")[2]
